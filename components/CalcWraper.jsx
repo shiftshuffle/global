97b7f25b3c6e752b8d82c950d1d56d92d123b061
francisco-villalobos <francisco.villalobos@sky.uk>
2023-04-25 10:00:52 -0600

    calc

--- components/CalcWraper.jsx
+++ components/CalcWraper.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Calculator from '@/components/Calculator3';
+// import Calculator from '@/components/Calculator3';
 const CalcWraper = () => {
     return (
         <div className="flex flex-wrap">
@@ -8,7 +8,7 @@ const CalcWraper = () => {
             </div>
             <div className="w-full md:w-1/2 bg-[#071646]">
                 <div className="p-10">
-                    <Calculator />
+                    {/* <Calculator /> */}
                 </div>
             </div>
 
